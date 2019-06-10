9a939ce9ea7ab5de35eb2d2572abf7a97cd0e5fe
avoronkov <aleksandrs.voronkovs@ideaportriga.lv>
2019-06-10 12:37:07 +0300

    Initial commit / cra cleanups

---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textTransform: 'uppercase',
+      }}
+    >
+      {' '}Init{' '}
+    </div>
+  );
+};
+
+export default App;

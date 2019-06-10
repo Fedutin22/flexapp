70658fff4a59c67c6b5408a32abbd0d9f9c70985
WarOnKhoff <voronkovs.aleksandrs@gmail.com>
2019-06-10 20:37:49 +0300

    Added global style components / added color palette

---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Column = styled.div`
+  display: flex;
+  flex-flow: column;
+  box-sizing: border-box;
+  width: 100%;
+`;
+export const Row = styled.div`
+  display: flex;
+  width: 100%;
+  box-sizing: border-box;
+`;
+export const Button = styled.button`
+background-color: transparent;
+outline:none;
+border: 1px solid black;
+`;
---
+++ src/styles/palette.js
@@ -0,0 +1,11 @@
+// Color preferances
+export const paletteColor1 = '#00D0B5';
+export const paletteColor2 = '#FCFCFC';
+export const paletteColor3 = '#3F4045';
+export const paletteColor4 = '#2C5675';
+export const paletteColor5 = '#04395E';
+export const paletteColor6 = '#333';
+//Size preferances
+export const fontSizeDefault = '30px';
+export const fontSizeAppName = '100px';
+export const fontSizeSlogan = '30px';
